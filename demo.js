9442986671d3e0abf81c074f8ffa34f2e696a91a
Mike <korostelevm@gmail.com>
2021-11-11 14:35:17 -0500

    update script

--- demo.js
+++ demo.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 // const token = process.env.SLACK_TOKEN;
 // const web = new WebClient(token);
 const app = express()
-
+const nacl = require('tweetnacl');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -13,11 +13,37 @@ router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 const Monkey = require('monkey-typewriter')
 
+
+const verify_discord = ()=>{
+     
+
+// Your public key can be found on your application in the Developer Portal
+const PUBLIC_KEY = 'b4e179f42c1fc742c9094e2af2c275da05db43b4ce62524c2e2760b564375fa4';
+
+
+}
 router.post('/discord_events', (req, res) => {
      console.log('discord event')
      console.log(req.headers)
      console.log(req.body)
-     res.sendStatus(200)
+
+     const signature = req.get('X-Signature-Ed25519');
+     const timestamp = req.get('X-Signature-Timestamp');
+     const body = req.rawBody; // rawBody is expected to be a string, not raw bytes
+
+     const isVerified = nacl.sign.detached.verify(
+     Buffer.from(timestamp + body),
+     Buffer.from(signature, 'hex'),
+     Buffer.from(PUBLIC_KEY, 'hex')
+     );
+     console.log(isVerified)
+
+     if (!isVerified) {
+     return res.status(401).end('invalid request signature');
+     }
+
+
+     return res.sendStatus(200)
 })
 
 router.get('/', (req, res) => {
