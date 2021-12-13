4d337b1307428ab6b6e9f95448d0319023f6e50d
Michael Korostelev <korostelevm@gmail.com>
2021-12-13 13:15:00 -0500

    Update demo.js

--- demo.js
+++ demo.js
@@ -52,8 +52,8 @@ router.get('/', (req, res) => {
      console.log(req.query)
      console.log(new Date())
      console.log([1,2,3,4,5])
-     console.log(express)
      console.log(Buffer.from('asdf'))
+     console.log(Monkey.word)
      let w  = Monkey.word()
      let l = w.length+1 - 4;
      let fill = (c,n)=>{
